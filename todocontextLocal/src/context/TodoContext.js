3e4da6e6d61ae3753ee9332faf98aab3c8a8591a
KamawalaShreya <shreyakamawala1106@gmail.com>
2024-02-25 23:10:44 +0530

    all practical projects

--- todocontextLocal/src/context/TodoContext.js
+++ todocontextLocal/src/context/TodoContext.js
@@ -1,1 +1,9 @@
 import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({});
+
+export const TodoContextProvider = TodoContext.Provider;
+
+export default useTodo = () => {
+  return useContext(TodoContext);
+};
